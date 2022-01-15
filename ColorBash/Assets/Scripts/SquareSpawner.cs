ebab13a472904af403cad8b271d89ab9264b23f8
myong003 <matthew.yong0801@gmail.com>
2022-01-15 15:21:11 -0800

    Finished triangle enemy

--- ColorBash/Assets/Scripts/SquareSpawner.cs
+++ ColorBash/Assets/Scripts/SquareSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SquareSpawner : MonoBehaviour
 {
 	public Square sq;
-
+    public Triangle tri;
 	public float startSpeed;
 	public float speedIncrease;
     public float speedIncreaseFrequency;
@@ -41,11 +41,27 @@ public class SquareSpawner : MonoBehaviour
         return returnColor;
     }
 
+    void spawnNewShape(){
+        int randShape = Random.Range(0, 2);
+        // Debug.Log(randShape);
+        switch (randShape){
+            case 0:
+                Square newSquare = Instantiate(sq, gameObject.transform.position, Quaternion.identity);
+                newSquare.GetComponent<SquareMover>().squareSpeed = startSpeed + (speedIncrease * numIncreases);
+                newSquare.GetComponent<SpriteRenderer>().color = getRandomColor();
+                break;
+            case 1:
+                Triangle newTri = Instantiate(tri, gameObject.transform.position, Quaternion.identity);
+                newTri.GetComponent<SquareMover>().squareSpeed = startSpeed + (speedIncrease * numIncreases);
+                newTri.GetComponent<SpriteRenderer>().color = getRandomColor();
+                break;
+        }
+
+    }
+
 	void Update(){
 		if (spawnTimer > timeBetweenSpawns){
-            Square newSquare = Instantiate(sq, gameObject.transform.position, Quaternion.identity);
-            newSquare.GetComponent<SquareMover>().squareSpeed = startSpeed + (speedIncrease * numIncreases);
-            newSquare.GetComponent<SpriteRenderer>().color = getRandomColor();
+            spawnNewShape();
             spawnTimer = 0;
 		}
         else{
