59f331b66f8503f8e7ebc09ee57496e53809d950
myong003 <matthew.yong0801@gmail.com>
2022-01-15 13:22:21 -0800

    added random colors for spawned squares

--- ColorBash/Assets/Scripts/SquareSpawner.cs
+++ ColorBash/Assets/Scripts/SquareSpawner.cs
@@ -15,10 +15,32 @@ public class SquareSpawner : MonoBehaviour
         spawnTimer = 0;
     }
 
+    Color getRandomColor(){
+        int randColor = Random.Range(0, 3);
+        Color returnColor = Color.white;
+        Debug.Log(randColor);
+        switch (randColor){
+            case 0:
+                returnColor = Color.red;
+                break;
+            case 1:
+                returnColor = Color.blue;
+                break;
+            case 2:
+                returnColor = Color.green;
+                break;
+            default:
+                Debug.Log("Color out of range");
+                break;
+        }
+        return returnColor;
+    }
+
 	void Update(){
 		if (spawnTimer > timeBetweenSpawns){
             Instantiate(sq, gameObject.transform.position, Quaternion.identity);
             sq.GetComponent<SquareMover>().squareSpeed = startSpeed;
+            sq.GetComponent<SpriteRenderer>().color = getRandomColor();
             spawnTimer = 0;
 		}
         else{
