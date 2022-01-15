065b19c1f2b155878c25daa861c4cd68ba887226
myong003 <matthew.yong0801@gmail.com>
2022-01-15 14:32:31 -0800

    Finished color buttons and added prefab for canvas

--- ColorBash/Assets/Scripts/CircleScript.cs
+++ ColorBash/Assets/Scripts/CircleScript.cs
@@ -14,4 +14,15 @@ public class CircleScript : MonoBehaviour
         Debug.Log("Setting color to red");
         sp.color = Color.red;
     }
+
+    public void setColorBlue(){
+        Debug.Log("Setting color to blue");
+        sp.color = Color.blue;
+    }
+
+    public void setColorGreen(){
+        Debug.Log("Setting color to green");
+        sp.color = Color.green;
+    }
+
 }
