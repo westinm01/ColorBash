28295c9a2c20f22f6638f24e84076e57a119bc22
brentnicks <brent_nicks@msn.com>
2022-01-15 17:13:24 -0800

    5:13

--- ColorBash/Assets/Scripts/Save Scripts/SaveType.cs
+++ ColorBash/Assets/Scripts/Save Scripts/SaveType.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class SaveType
 {
     public int points;
+    public int highScore;
+    public bool[] circles = { true, false, false, false, false, false, false, false, false, false, false, false };
+    public bool[] backgrounds = {true, false, false };
 
-    public SaveType(int points_)
+    public SaveType(int points_, bool[] circles_, bool[] backgrounds_, int highScore_)
     {
         points = points_;
+        circles = circles_;
+        backgrounds = backgrounds_;
+        highScore = highScore_;
+        
     }
 }
