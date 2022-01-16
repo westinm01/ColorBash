5c2534bd406cc6bc31b098d24dea54def35ff56b
myong003 <matthew.yong0801@gmail.com>
2022-01-15 19:49:18 -0800

    Added game over screen

--- ColorBash/Assets/Scripts/GameManager.cs
+++ ColorBash/Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public bool hasStarted;
+    public GameObject GameOverScreen;
 
     public void StartGame(){
         hasStarted = true;
@@ -12,8 +14,20 @@ public class GameManager : MonoBehaviour
 
     public void GameOver(){
         Debug.Log("Game Over");
+        GameOverScreen.SetActive(true);
         hasStarted = false;
         Time.timeScale = 0;
+    }
+
+    public void Exit(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Restart(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartGame();
     }
 
     // Start is called before the first frame update
