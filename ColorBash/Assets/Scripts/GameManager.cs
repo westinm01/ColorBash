671c9188788c7e7cb14f2ad8768bade935711366
myong003 <matthew.yong0801@gmail.com>
2022-01-16 02:26:19 -0800

    fixed new high score screen

--- ColorBash/Assets/Scripts/GameManager.cs
+++ ColorBash/Assets/Scripts/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         hasStarted = false;
-        newHighScore = false;
+        newHighScore = true;
         SaveData.LoadInfo();
         SaveData.SaveInfo();
     }
