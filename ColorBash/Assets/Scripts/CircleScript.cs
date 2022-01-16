73da640d6db7126b29a56dcd2ca9507f25889c88
myong003 <matthew.yong0801@gmail.com>
2022-01-15 16:27:03 -0800

    Changed triangles and squares to triggers

--- ColorBash/Assets/Scripts/CircleScript.cs
+++ ColorBash/Assets/Scripts/CircleScript.cs
@@ -33,8 +33,8 @@ public class CircleScript : MonoBehaviour
     }
 
     void Update(){
-        gameObject.transform.RotateAround(gameObject.transform.position + new Vector3(0.25f, 0, 0), new Vector3(0, 0, 1), -rotationSpeed);
-        gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0.0001f, 0);
+        gameObject.transform.RotateAround(gameObject.transform.position, new Vector3(0, 0, 1), -rotationSpeed);
+        // gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(0.0001f, 0);
         // gameObject.transform.position += new Vector3(0.1f, 0);
     }
 
---
+++ ColorBash/Assets/Scripts/TriangleTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleTrigger : SquareTrigger
+{
+    protected override void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.tag == "Player"){
+            Debug.Log("Collided with Player");
+            if (collision.gameObject.GetComponent<SpriteRenderer>().color != square.color){
+                ScoreScript.scoreValue += 1; 
+                takeDamage();
+            }
+            else{
+                Debug.Log("Game over");
+            }
+		}
+	}
+}
